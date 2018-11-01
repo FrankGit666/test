02b689df1b1e1e948d0cbe3a7f039c7d1a2e099f
frank <1163858300@qq.com>
2018-11-01 23:27:55 +0800

    chanke/test/修改

---
+++ chanke/test/index.js
@@ -0,0 +1,14 @@
+console.log("start");
+working(10*1000);
+console.log("end");
+
+
+
+function working(time){
+    var nowTime = Date.now();
+    var exitTime = nowTime + time;
+
+    while(nowTime < exitTime){
+        nowTime = Date.now();
+    }
+}
--- chanke/test/test.js
+++
@@ -1,7 +0,0 @@
-var p1 = document.getElementById("p1");
-p1.textContent = "这是innerHTML <a href='http://www.baidu.com'> this is a</a>"    //这是修改纯文本
-var ul1 = document.getElementsByTagName("ul")[0];
-ul1.addEventListener("click",ul1Click);
-function ul1Click(event){
-    console.log(event.target);
-}
